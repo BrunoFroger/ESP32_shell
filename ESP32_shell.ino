5bc211345360baf4c131a85a13269ffe606d6b18
Bruno FROGER <bruno.froger@orange.com>
2020-02-11 16:04:17 +0100

    first version

---
+++ ESP32_shell.ino
@@ -0,0 +1,45 @@
+//
+//  module de test de la feather ESP32
+//  test de création d'un serveur ssh 
+//  afin de se connecter a l'esp32 en ssh 
+// (c) B. Froger 2020
+
+
+#include <arduino.h>
+#include <WiFi.h>
+
+
+//=========================================
+//
+//          setup
+//
+//=========================================
+void setup() {
+    delay(2000);
+    // initialize serial communication
+    Serial.begin(115200);
+    int timeoutInitSerial = 100;
+    while (timeoutInitSerial-- > 0)
+    {
+        if (Serial)
+        break;
+        delay(10);
+    }
+    delay(5000);
+    if (timeoutInitSerial != 0)
+    {
+        Serial.println("Serial initialized");
+    } else
+    {
+        Serial.println("Serial not initialized");
+        exit(0);
+    }
+
+    Serial.println("debut de setup");
+    initWifi();
+    Serial.println("Init Wifi OK");
+
+    delay(1000);
+
+    Serial.println("Fin de setup");
+}
---
+++ wifi.cpp
@@ -0,0 +1,18 @@
+
+
+#include <Arduino.h>
+#include <Wifi.h>
+
+const char* ssid = "yourNetworkName";
+const char* password = "yourNetworkPassword";
+
+
+//=========================================
+//
+//          initWifi
+//
+//=========================================
+void initWifi(void){    // init wifi connection
+
+}
+
